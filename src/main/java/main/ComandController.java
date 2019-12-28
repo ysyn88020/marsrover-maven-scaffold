b25e430505b6ee73fa01bb91800d449d10f63494
fanliwen <fanliwen@163.com>
2019-12-28 17:26:54 +0800

    refactor

---
+++ src/main/java/main/ComandController.java
@@ -0,0 +1,23 @@
+package main;
+
+/**
+ * @author f_liwen
+ * @version 1.0
+ * @date 2019/12/28 16:24
+ */
+public class ComandController {
+    public String excute(String command, Rover rover) {
+
+        String a[]=command.split(":");
+        for(String b:a){
+            if("M".equals(b)){
+                rover.move();
+            }else if("L".equals(b)){
+                rover.turnLeft();
+            }else if("R".equals(b)){
+                rover.turnRight();
+            }
+        }
+        return String.valueOf(rover.getX())+String.valueOf(rover.getY())+rover.getDirection();
+    }
+}
